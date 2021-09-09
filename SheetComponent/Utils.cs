db2f6e30700119e90698c844284d0110fb0f752b
Pognant Gros Alberto <A.PognantGros@almaviva.it>
2021-09-09 08:57:18 +0200

    Prima commit

---
+++ SheetComponent/Utils.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SheetComponent
+{
+    public static class Utils
+    {
+        public static string GetColumnLabel(int id, string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ") => (id > letters.Length ? GetColumnLabel(id / letters.Length) : "") + letters[id % letters.Length];
+    }
+}
