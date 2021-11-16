5639f12fbb2a01230cfe29e63bb14bdccc6540d6
Pognant Gros Alberto <A.PognantGros@almaviva.it>
2021-11-16 15:18:06 +0100

    Work in progress: Cambiamento strategia rendering da TABLE a DIV con position:absolute

--- SheetComponent/Models.cs
+++ SheetComponent/Models.cs
@@ -9,18 +9,34 @@ namespace SheetComponent
 {
     public struct Position
     {
+        /// <summary>La posizione che corrisponde all'incrocio degli assi di intestazione righe/colonne. Indica anche la dimensione di un range nullo (zero righe x zero colonne)</summary>
+        public static Position OrigineAssi = (0, 0);
+        /// <summary>La posizione che corrisponde alla prima cella in alto a sinistra (A1). Indica anche la dimensione di un range di una singola cella (1 riga x 1 colonna)</summary>
+        public static Position Unit = (1, 1);
+
         public int Row { get; set; }
         public int Col { get; set; }
 
-        public override string ToString() => Utils.GetColumnLabel(Col) + Row.ToString();
+        public bool IsIntestazioneRiga { get => Col == 0 && Row > 0; }
+        public bool IsIntestazioneColonna { get => Row == 0 && Col > 0; }
+
+        public bool InRange(Range r) =>
+               (r.Start.IsIntestazioneColonna || (Row >= r.Top && Row <= r.Bottom))
+            && (r.Start.IsIntestazioneRiga || (Col >= r.Left && Col <= r.Right));
+
+        public override string ToString() => Utils.GetColumnLabel(Col) + (Row==0?"":Row.ToString());
 
         public static bool operator == (Position a, Position b) => a.Row == b.Row && a.Col == b.Col;
         public static bool operator !=(Position a, Position b) => !(a==b);
+
+        public static Position operator +(Position a, Position b) => new Position() { Row = a.Row + b.Row, Col = a.Col + b.Col };
 
         public override bool Equals(object obj) => obj is Position other && this == other;
         public override int GetHashCode() => (Row, Col).GetHashCode();
 
         public static implicit operator Position((int, int) pos) => new Position() { Row = pos.Item1, Col = pos.Item2 };
+        //public static implicit operator (int, int)(Position pos) => (pos.Row, pos.Col);
+
         public void Deconstruct(out int row, out int col) => (row, col) = (Row, Col);
     }
 
@@ -29,19 +45,40 @@ namespace SheetComponent
         public Position Start { get; set; }
         public Position End { get; set; }
 
+        /// <summary>Restituisce il valore della riga più in alto (con numero di riga più basso)</summary>
         public int Top { get => Math.Min(Start.Row, End.Row); }
+
+        /// <summary>Restituisce il valore della riga più in basso (con numero di riga più alto)</summary>
         public int Bottom { get => Math.Max(Start.Row, End.Row); }
+
+        /// <summary>Restituisce il valore della colonna più a sinistra (con la lettera più bassa)</summary>
         public int Left { get => Math.Min(Start.Col, End.Col); }
+
+        /// <summary>Restituisce il valore della colonna più a destra (con la lettera più alta)</summary>
         public int Right { get => Math.Max(Start.Col, End.Col); }
 
-        public int Height { get => Bottom - Top + 1; }
-        public int Width { get => Right - Left + 1; }
+        /// <summary>Restituisce il numero di righe totali</summary>
+        private int Height { get => Top == 0 ? 0 : (Bottom - Top + 1); }
+
+        /// <summary>Restituisce il numero di colonne totali</summary>
+        private int Width { get => Left == 0 ? 0 : Right - Left + 1; }
+
+        /// <summary>Dimensione del range. Proprietà di 3° Livello: se possibile, usare <see cref="Height" /> e <see cref="Width" />, oppure <see cref="Top"/>, <see cref="Bottom"/>, <see cref="Left"/>, <see cref="Right"/></summary>
+        public Position Size { get => new Position() { Row = Height, Col = Width }; }
+
+        public override string ToString() => $"{(Position)(Top, Left)}:{(Position)(Bottom, Right)}";
+
 
     }
 
     public class Cell
     {
+        private Cell() { }
+        public Cell(Position pos) { Position = pos; }
+        public Cell(Range merge) { Position = merge.Start; Merge = merge; }
+
         public Position Position { get; set; }
+        public Range Merge { get; set; }
         public string Value { get; set; }
     }
 
--- SheetComponent/Sheet.razor.cs
+++ SheetComponent/Sheet.razor.cs
@@ -12,73 +12,116 @@ namespace SheetComponent
     {
         [Parameter] public int Rows { get; set; } = 20;
         [Parameter] public int Columns { get; set; } = 10;
+        [Parameter] public bool UseTables { get; set; } = false;
 
         private List<Cell> SheetData = new();
 
         private ElementReference CellInput { get; set; }
         private ElementReference StatusBar { get; set; }
+        private ElementReference SheetDiv { get; set; }
 
-        public Range Selection { get; private set; }
+        public Range Selection { get; private set; } = new() { Start = Position.Unit, End = Position.Unit };
+        public Range WorkingArea { get; private set; }
 
         private bool MouseDown;
-        private Position CurrentPosition;
-        private Position PreviousPosition;
+        private MouseEventArgs LastMouseEvent;
+        private bool KeyDown;
+        private KeyboardEventArgs LastKeyEvent;
+        private bool InEdit;
+        private Position CurrentPosition = Position.Unit;
 
-        private Position StartPosition;
-        private Position EndPosition;
-        private async Task SetSelection(Position? start = null, Position? end = null)
+        private Position StartPosition = Position.Unit;
+        private Position EndPosition = Position.Unit;
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await SheetDiv.FocusAsync(); // permette al div principale di intercettare tutti gli eventi da tastiera
+        }
+
+        protected override void OnParametersSet()
+        {
+            WorkingArea = new() { Start = Position.Unit, End = (Rows, Columns) };
+        }
+
+        private void SetSelection(Position? start = null, Position? end = null)
         {
             if (start != null)
             {
-                CurrentPosition = 
+                //CurrentPosition = 
                 StartPosition = start.Value;
                 if (end == null) EndPosition = start.Value;
             }
             if (end != null) EndPosition = end.Value;
             Selection = new Range() { Start = StartPosition, End = EndPosition };
-            if (CellInput.Context != null) await CellInput.FocusAsync();
+            //if (CellInput.Context != null) await CellInput.FocusAsync();
         }
 
         private void CellClick(Position pos, MouseEventArgs args)
         {
-            if (args.Button == 0)
-            {
-                //CurrentPosition = pos;
-            }
+            LastMouseEvent = args;
+            //if (args.Button == 0)
+            //{
+            //    CurrentPosition = pos;
+            //}
         }
 
-        private async void CellMouseDown(Position pos, MouseEventArgs args)
+        private void CellMouseDown(Position pos, MouseEventArgs args)
         {
+            LastMouseEvent = args;
             if (args.Button == 0)
             {
-                if (pos != CurrentPosition) // sto cambiando cella: se ci sono digitazioni non salvate, le consolido
-                {
-
-                }
                 MouseDown = true;
-                PreviousPosition = CurrentPosition;
-                await SetSelection(start: pos);
+                if (pos == Position.OrigineAssi)
+                {
+                    CurrentPosition = (1, 1); //TODO: impostare all'origne della viewport
+                    SetSelection(pos);
+                    MouseDown = false; // interrompo l'ulteriore selezione di qualunque altra cosa
+                }
+                else if (pos.IsIntestazioneRiga)
+                {
+                    CurrentPosition = (pos.Row, 1); //TODO: impostare la colonna alla prima visibile della viewport
+                    SetSelection((pos.Row, 0)); // seleziono intera riga
+                }
+                else if (pos.IsIntestazioneColonna)
+                {
+                    CurrentPosition = (1, pos.Col); //TODO: impostare la riga alla prima visibile della viewport
+                    SetSelection((0, pos.Col)); // seleziono intera colonna
+                }
+                else
+                {
+                    if (args.ShiftKey) SetSelection(end: pos);
+                    else
+                    {
+                        CurrentPosition = pos;
+                        SetSelection(start: pos);
+                    }
+                }
             }
         }
 
-        private async void CellMouseUp(Position pos, MouseEventArgs args)
+        private void CellMouseUp(Position pos, MouseEventArgs args)
         {
-            if (args.Button == 0)
+            LastMouseEvent = args;
+            if (MouseDown && args.Button == 0)
             {
                 MouseDown = false;
-                await SetSelection(end: pos);
-                if (EndPosition != StartPosition)
+                if (args.ShiftKey)
                 {
-                    CurrentPosition = StartPosition; // mouse up e mouse down in celle diverse => l'evento click non viene sollevato e devo sopperire
+                    if (pos.InRange(WorkingArea)) SetSelection(end: pos);
                 }
+                //SetSelection(end: pos);
+                //if (EndPosition != StartPosition)
+                //{
+                //    CurrentPosition = StartPosition; // mouse up e mouse down in celle diverse => l'evento click non viene sollevato e devo sopperire
+                //}
             }
         }
 
-        private async void CellMouseMove(Position pos, MouseEventArgs args)
+        private void CellMouseMove(Position pos, MouseEventArgs args)
         {
             if (MouseDown)
             {
-                await SetSelection(end: pos);
+                SetSelection(end: pos);
                 if (EndPosition != StartPosition)
                 {
                     CurrentPosition = StartPosition; // mouse up e mouse move in celle diverse => l'evento click non viene sollevato e devo sopperire
@@ -86,29 +129,81 @@ namespace SheetComponent
             }
         }
 
-        private Cell GetCell(Position pos, bool createNew = false)
+        private void SheetKeyDown(KeyboardEventArgs args)
         {
-            var cell = SheetData.Find(x => x.Position == pos);
-            if (cell is null && createNew) SheetData.Add(cell = new() { Position = pos });
-            return cell;
-        }
-
-        private IEnumerable<string> getTdClasses(Position pos)
-        {
-            if (pos.Col >= Selection.Left && pos.Col <= Selection.Right && pos.Row >= Selection.Top && pos.Row <= Selection.Bottom)
+            KeyDown = true;
+            LastKeyEvent = args;
+            Action<Position> moveFunc = args.ShiftKey ? ExtendCurrentSelection : MoveCurrentPosition;
+            switch (args.Key)
             {
-                if (pos != Selection.Start) yield return "selected";
-                if (pos.Row == Selection.Top) yield return "selection-top";
-                if (pos.Row == Selection.Bottom) yield return "selection-bottom";
-                if (pos.Col == Selection.Left) yield return "selection-left";
-                if (pos.Col == Selection.Right) yield return "selection-right";
+                case "ArrowLeft": moveFunc((0, -1)); break;
+                case "ArrowRight": moveFunc((0, 1)); break;
+                case "ArrowUp": moveFunc((-1, 0)); break;
+                case "ArrowDown": moveFunc((1, 0)); break;
             }
         }
 
-        private IEnumerable<string> getThClasses(Position pos)
+        private void MoveCurrentPosition(Position delta)
         {
-            if (pos.Col == 0) yield return "vertical-ruler-cell";
+            var newpos = CurrentPosition + delta;
+            if (newpos.InRange(WorkingArea))
+            {
+                CurrentPosition = newpos;
+                SetSelection(newpos);
+            }
+            else
+            {
+                SetSelection(CurrentPosition); // per emulare il comportamento di excel, reimposto la selezione sulla cella corrente senza spostarmi
+            }
         }
 
+        private void ExtendCurrentSelection(Position delta)
+        {
+            var newEnd = Selection.End + delta;
+            if (newEnd.InRange(WorkingArea)) SetSelection(end: newEnd);
+        }
+
+            private Cell GetCell(Position pos, bool createNew = false)
+        {
+            var cell = SheetData.Find(x => x.Position == pos);
+            if (cell is null && createNew) SheetData.Add(cell = new(pos));
+            return cell;
+        }
+
+        private string GetCellClass(Position pos)
+        {
+            string resp;
+
+            if (pos == Position.OrigineAssi)
+            {
+                resp = "header";
+            }
+            else if (pos.IsIntestazioneRiga)
+            {
+                resp = "header row-header";
+                if (Selection.Top == 0 || (pos.Row >= Selection.Top && pos.Row <= Selection.Bottom)) resp += " selected";
+                if (Selection.Left == 0) resp += " full";
+            }
+            else if (pos.IsIntestazioneColonna)
+            {
+                resp = "header col-header";
+                if (Selection.Left == 0 || (pos.Col >= Selection.Left && pos.Col <= Selection.Right)) resp += " selected";
+                if (Selection.Top == 0) resp += " full";
+            }
+            else // area dati (riga e colonna > 0)
+            {
+                resp = "cell";
+                if (pos.InRange(Selection) || Selection.Start == Position.OrigineAssi)
+                {
+                    if (pos != CurrentPosition) resp += " selected";
+                    if (pos.Row == Selection.Top || (Selection.Start.Row == 0 && pos.Row == 1)) resp += " selection-top";
+                    if (pos.Row == Selection.Bottom) resp += " selection-bottom";
+                    if (pos.Col == Selection.Left || (Selection.Start.Col == 0 && pos.Col == 1)) resp += " selection-left";
+                    if (pos.Col == Selection.Right) resp += " selection-right";
+                }
+            }
+
+            return resp;
+        }
     }
 }
